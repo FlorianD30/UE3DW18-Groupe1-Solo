ee1d72ae258ec8b0c165d7376b207ebb7d6dfd75
BSP <baptiste.saint-pierre.aff@unilim.fr>
2020-11-22 11:14:42 +0100

    Poney

--- app/config/prod.php
+++ app/config/prod.php
@@ -4,11 +4,11 @@
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => 'mysql',
+    'host'     => 'localhost',
     'port'     => '3306',
-    'dbname'   => 'docker',
-    'user'     => 'docker',
-    'password' => 'docker'
+    'dbname'   => 'CHANGE HERE YOUR DB NAME',
+    'user'     => 'CHANGE HERE YOUR USER',
+    'password' => 'CHANGE HERE YOUR PASSWORD'
 );
 
 // define log parameters
